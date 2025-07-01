d104275c14b437834014fd21540d7c59bdec7327
Shreya Patel <shreya patel>
2025-07-01 14:27:21 +0530

    Initial commit

--- home.py
+++ home.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('dash.html')
-@app.route("/login")
-def login():
-    return render_template('login.html')
-@app.route("/register")
-def register():
-    return render_template('register.html')
+# @app.route("/login")
+# def login():
+    # return render_template('login.html')
+# @app.route("/register")
+# def register():
+    # return render_template('register.html')
 app.run(debug=True) 
 
 # Define only the main topics with basic responses
